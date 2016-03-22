514a09182e5aadbd0913ee430532ddacad2fe514
Raj Mannar <raj.mannar@aa.com>
2016-03-22 03:11:55 +0000

    Modified test script

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -196,7 +196,7 @@ describe UsersController do
 
       it "should have a welcome message" do
         post :create, :user => @attr
-        flash[:success].should =~ /welcome to the sample app/i
+        flash[:success].should =~ /welcome to the CHANGE sample app/i
       end
       
       it "should sign the user in" do
